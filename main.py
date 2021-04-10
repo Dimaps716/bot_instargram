56820bfd8774b0a800f797e5333fb32bed367cf2
Dima <dimaps716@gmail.com>
2021-04-09 22:01:02 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from instabot import Bot
+
+
+bot = Bot()
+
+bot.login(username="dimanson64", password="dima1994.")
+bot.follow_followers("cuenta que quieres segir")
+bot.send_message["mensaje",["usuarios"]]
